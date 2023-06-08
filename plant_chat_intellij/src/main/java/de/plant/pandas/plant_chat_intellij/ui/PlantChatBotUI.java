91c985b4adc86991bc6b49eec37a50081b909112
Jesse Palarus <jtsharkjtshark@gmail.com>
2023-06-08 06:56:17 +0200

    make textfield bigger

--- plant_chat_intellij/src/main/java/de/plant/pandas/plant_chat_intellij/ui/PlantChatBotUI.java
+++ plant_chat_intellij/src/main/java/de/plant/pandas/plant_chat_intellij/ui/PlantChatBotUI.java
@@ -22,74 +22,103 @@ import de.plant.pandas.plant_chat_intellij.ui.settings.PlantChatSettings;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PlantChatBotUI implements ToolWindowFactory {
+
+
+    private void generateUML(Project project, String input) {
+        UMLChatBot umlChatBot = new UMLChatBotImpl(new OpenAILLM(PlantChatSettings.getInstance().openAIToken));
+
+        Collection<VirtualFile> currentDiagramsFiles = PlantChatHelper.findPumlFiles(project);
+
+        Collection<String> currentDiagramStrings = currentDiagramsFiles.stream().map((file) -> {
+            try {
+                return file.getName() + "\n" + VfsUtil.loadText(file);
+            } catch (IOException ex) {
+                return "";
+            }
+        }).collect(Collectors.toList());
+
+
+        Map<String, String> answer = umlChatBot.askQuestion(currentDiagramStrings, input);
+
+        VirtualFile baseDir =
+                ProjectUtil.guessProjectDir(project);
+        if (baseDir != null) {
+
+
+            ApplicationManager.getApplication().runWriteAction(() -> {
+                for (Map.Entry<String, String> filenameUmlDiagramPair : answer.entrySet()) {
+
+                    String filename = filenameUmlDiagramPair.getKey();
+                    String uml = filenameUmlDiagramPair.getValue();
+
+                    PsiDirectory psiDirectory = PsiManager.getInstance(project).findDirectory(baseDir);
+
+                    PsiFile psiFile = psiDirectory.findFile(filename);
+                    if (psiFile != null) {
+                        Document document = PsiDocumentManager.getInstance(project).getDocument(psiFile);
+                        if (document != null) {
+                            document.setText(uml);
+                        }
+                    } else {
+                        psiFile = PsiFileFactory.getInstance(project).createFileFromText(filename, FileTypeManager.getInstance().getFileTypeByExtension("puml"), uml);
+                        psiFile = (PsiFile) psiDirectory.add(psiFile);
+                    }
+
+
+                    VirtualFile virtualFile = psiFile.getVirtualFile();
+                    if (virtualFile != null) {
+                        FileEditorManager.getInstance(project).openFile(virtualFile, true);
+                    }
+                }
+            });
+
+
+        }
+    }
+
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        // Create a panel with a text field
+
         JPanel panel = new JPanel();
-        JTextField textField = new JTextField(20);
-        textField.addActionListener((e) -> {
-            String text = textField.getText();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        JPanel textAreaPanel = new JPanel(new BorderLayout());
+        textAreaPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
 
-            UMLChatBot umlChatBot = new UMLChatBotImpl(new OpenAILLM(PlantChatSettings.getInstance().openAIToken));
-            textField.setText("");
+        JTextArea textArea = new JTextArea();
 
-            Collection<VirtualFile> currentDiagramsFiles = PlantChatHelper.findPumlFiles(project);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
 
-            Collection<String> currentDiagramStrings = currentDiagramsFiles.stream().map((file) -> {
-                try {
-                    return file.getName() + "\n" + VfsUtil.loadText(file);
-                } catch (IOException ex) {
-                    return "";
-                }
-            }).collect(Collectors.toList());
+        textArea.setBorder(BorderFactory.createCompoundBorder(
+                textArea.getBorder(),
+                BorderFactory.createEmptyBorder(0, 8, 0, 8)));
 
+        textAreaPanel.add(textArea, BorderLayout.NORTH);
+        panel.add(textAreaPanel);
 
-            Map<String, String> answer = umlChatBot.askQuestion(currentDiagramStrings, text);
-
-            VirtualFile baseDir =
-                    ProjectUtil.guessProjectDir(project);
-            if (baseDir != null) {
-
-
-                ApplicationManager.getApplication().runWriteAction(() -> {
-                    for (Map.Entry<String, String> filenameUmlDiagramPair : answer.entrySet()) {
-
-                        String filename = filenameUmlDiagramPair.getKey();
-                        String uml = filenameUmlDiagramPair.getValue();
-
-                        PsiDirectory psiDirectory = PsiManager.getInstance(project).findDirectory(baseDir);
-
-                        PsiFile psiFile = psiDirectory.findFile(filename);
-                        if (psiFile != null) {
-                            Document document = PsiDocumentManager.getInstance(project).getDocument(psiFile);
-                            if (document != null) {
-                                document.setText(uml);
-                            }
-                        } else {
-                            psiFile = PsiFileFactory.getInstance(project).createFileFromText(filename, FileTypeManager.getInstance().getFileTypeByExtension("puml"), uml);
-                            psiFile = (PsiFile) psiDirectory.add(psiFile);
-                        }
-
-
-                        VirtualFile virtualFile = psiFile.getVirtualFile();
-                        if (virtualFile != null) {
-                            FileEditorManager.getInstance(project).openFile(virtualFile, true);
-                        }
+        textArea.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (e.isShiftDown()) {
+                        textArea.append("\n");
+                    } else {
+                        String text = textArea.getText();
+                        generateUML(project, text);
+                        textArea.setText("");
+                        e.consume();
                     }
-                });
-
-
+                }
             }
-
         });
-        panel.add(textField);
 
         // Create a content with the panel and add it to the tool window
         ContentFactory contentFactory = ContentFactory.getInstance();
