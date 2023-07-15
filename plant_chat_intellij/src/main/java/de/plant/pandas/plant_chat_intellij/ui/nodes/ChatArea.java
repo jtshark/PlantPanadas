b674cb21c01f397001f09a7ea45e293557797817
Jesse Palarus <jtsharkjtshark@gmail.com>
2023-07-15 14:58:24 +0200

    add scrolling

--- plant_chat_intellij/src/main/java/de/plant/pandas/plant_chat_intellij/ui/nodes/ChatArea.java
+++ plant_chat_intellij/src/main/java/de/plant/pandas/plant_chat_intellij/ui/nodes/ChatArea.java
@@ -1,15 +1,29 @@
 package de.plant.pandas.plant_chat_intellij.ui.nodes;
 
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
-public class ChatArea extends VBox {
+public class ChatArea extends ScrollPane {
+    private final VBox chatBubblesBox;
+
     public ChatArea() {
+        chatBubblesBox = new VBox();
+        chatBubblesBox.setSpacing(10);
+        chatBubblesBox.setPadding(new Insets(10));
+
+        setContent(chatBubblesBox);
+        setFitToWidth(true);
+        setVvalue(1.0); // scroll to bottom
+        vvalueProperty().bind(chatBubblesBox.heightProperty()); // auto scroll to bottom when a new message is added
+
         VBox.setVgrow(this, Priority.ALWAYS);
     }
 
     public void addChatMessage(String message, boolean isUser) {
         ChatBubble chatBubble = new ChatBubble(message, isUser);
-        getChildren().add(chatBubble);
+        chatBubblesBox.getChildren().add(chatBubble);
     }
 }
